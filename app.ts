71309284eb8219e1a75facb30c773c5c32b8a77a
indiandevtm <rajarockromeo@gmail.com>
2023-04-13 10:20:41 +0530

    ts calculate fn

---
+++ app.ts
@@ -0,0 +1,14 @@
+let num1 : number = 9
+let num2: number = 9
+/* Method 1 */
+function addNumber (num1, num2) { 
+   return num1 + num2;
+};
+console.log(addNumber(num1, num2))
+/* Method 2 */
+const no1 = 5
+const no2 = 15
+function calculate (no1 : number, no2: number) { 
+   return no1 + no2;
+};
+console.log(calculate(no1,no2))
